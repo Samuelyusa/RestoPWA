048c4530b6c6231a331a43a635a412885110a388
Samuel Yusa <Samuelyusa@gmail.com>
2023-05-27 23:16:31 +0700

    Cache API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,6 +2,7 @@ const CONFIG = {
     BASE_URL: 'https://restaurant-api.dicoding.dev',
     BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
     ICON: '../../images/icon/',
+    CACHE_NAME: 'RestoCatalog',
 };
 
 export default CONFIG;
